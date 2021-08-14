22ab9c93e439e10ddc8ec085aa1c6b2f0613baca
Gareth Coles <gdude2002@gmail.com>
2021-08-14 13:49:42 +0100

    Update all the template deps

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,10 @@
 pluginManagement {
     plugins {
         // Update this in libs.version.toml when you change it here
-        kotlin("jvm") version "1.5.10"
+        kotlin("jvm") version "1.5.20"
 
         // Update this in libs.version.toml when you change it here
-        id("io.gitlab.arturbosch.detekt") version "1.17.1"
+        id("io.gitlab.arturbosch.detekt") version "1.18.0"
 
         id("com.github.jakemarsden.git-hooks") version "0.0.1"
         id("com.github.johnrengelman.shadow") version "5.2.0"
