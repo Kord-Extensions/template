6127954bd3ee57ed31ed1b4d5cf847346e411636
Ash <ash.b.7558@gmail.com>
2021-08-19 13:08:26 +0100

    Update KordEx Extension.kt

--- .idea/fileTemplates/KordEx Extension.kt
+++ .idea/fileTemplates/KordEx Extension.kt
@@ -3,9 +3,10 @@
 #end
 import com.kotlindiscord.kord.extensions.extensions.Extension
 
+#set($extName = $NAME.toLowerCase())
 #parse("File Header.java")
-class ${NAME} : Extension() {
-    override val name = "${Extension_name}"
+class ${NAME}Extension : Extension() {
+    override val name = "$extName"
     
     override suspend fun setup() {
         TODO("Not yet implemented")
