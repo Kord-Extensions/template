c75173c6d069250fca54f9e5a240b5acc1fc25fa
Gareth Coles <gdude2002@gmail.com>
2021-09-08 14:21:47 +0100

    Update the template again

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 pluginManagement {
     plugins {
         // Update this in libs.version.toml when you change it here
-        kotlin("jvm") version "1.5.20"
+        kotlin("jvm") version "1.5.30"
 
         // Update this in libs.version.toml when you change it here
         id("io.gitlab.arturbosch.detekt") version "1.18.0"
