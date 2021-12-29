f1f43833ceabcbfea5096ec759b31d3fe6944fea
Gareth Coles <gdude2002@gmail.com>
2021-12-29 15:10:24 +0000

    Snapshots: Update to latest

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,11 @@
 pluginManagement {
     plugins {
         // Update this in libs.version.toml when you change it here
-        kotlin("jvm") version "1.5.30"
-        kotlin("plugin.serialization") version "1.5.30"
+        kotlin("jvm") version "1.6.10"
+        kotlin("plugin.serialization") version "1.6.10"
 
         // Update this in libs.version.toml when you change it here
-        id("io.gitlab.arturbosch.detekt") version "1.18.0"
+        id("io.gitlab.arturbosch.detekt") version "1.19.0"
 
         id("com.github.jakemarsden.git-hooks") version "0.0.1"
         id("com.github.johnrengelman.shadow") version "5.2.0"
