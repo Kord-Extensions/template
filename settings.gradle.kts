30c68a73a2b84633ac14007ec11192d87a216d77
Gareth Coles <gdude2002@gmail.com>
2024-01-12 13:23:09 +0000

    Update template deps/etc

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,11 @@
 pluginManagement {
 	plugins {
 		// Update this in libs.version.toml when you change it here.
-		kotlin("jvm") version "1.9.0"
-		kotlin("plugin.serialization") version "1.9.0"
+		kotlin("jvm") version "1.9.22"
+		kotlin("plugin.serialization") version "1.9.22"
 
 		// Update this in libs.version.toml when you change it here.
-		id("io.gitlab.arturbosch.detekt") version "1.23.1"
+		id("io.gitlab.arturbosch.detekt") version "1.23.4"
 
 		id("com.github.jakemarsden.git-hooks") version "0.0.1"
 		id("com.github.johnrengelman.shadow") version "8.1.1"
--- src/main/kotlin/template/extensions/TestExtension.kt
+++ src/main/kotlin/template/extensions/TestExtension.kt
@@ -7,7 +7,6 @@ import com.kotlindiscord.kord.extensions.commands.converters.impl.user
 import com.kotlindiscord.kord.extensions.extensions.Extension
 import com.kotlindiscord.kord.extensions.extensions.chatCommand
 import com.kotlindiscord.kord.extensions.extensions.publicSlashCommand
-import com.kotlindiscord.kord.extensions.types.respond
 import com.kotlindiscord.kord.extensions.utils.respond
 import template.TEST_SERVER_ID
 
