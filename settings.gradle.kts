c248bb81918c3cf33dd297b09f422ffc0a539249
Gareth Coles <gdude2002@gmail.com>
2024-10-20 16:32:21 +0100

    Update KordEx Gradle plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,8 +10,8 @@ pluginManagement {
 		id("com.github.jakemarsden.git-hooks") version "0.0.2"
 		id("com.github.johnrengelman.shadow") version "8.1.1"
 
-		id("dev.kordex.gradle.docker") version "1.5.0"
-		id("dev.kordex.gradle.kordex") version "1.5.0"
+		id("dev.kordex.gradle.docker") version "1.5.1"
+		id("dev.kordex.gradle.kordex") version "1.5.1"
 	}
 
 	repositories {
